555fd43d3c2956c9edf9142326d5f321fdd60b28
Kostich31 <k.v.kostinich@mail.ru>
2022-02-24 14:51:05 +0300

    lab1

--- lab/app/src/main/java/com/example/lab/MainActivity.java
+++ lab/app/src/main/java/com/example/lab/MainActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
 import com.example.lab.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,15 +27,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        int res = initRng();
+        byte[] rnd = randomBytes(10);
     }
 
+    public void onButtonClick(View v)
+    {
+        //Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+
+        byte[] key =
+                stringToHex("0123456789ABCDEF0123456789ABCDE0");
+        byte[] enc = encrypt(key,
+                stringToHex("000000000000000102"));
+        byte[] dec = decrypt(key, enc);
+        String s = new String(Hex.encodeHex(dec)).toUpperCase();
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+
+    }
+
+    public static byte[] stringToHex(String s)
+    {
+        byte[] hex;
+        try
+        {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
+    }
+
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
     public native String stringFromJNI();
     public static native int initRng();
     public static native byte[] randomBytes(int no);
     public static native byte[] encrypt(byte[] key, byte[] data);
     public static native byte[] decrypt(byte[] key, byte[] data);
+
 }
